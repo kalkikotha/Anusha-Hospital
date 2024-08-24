abbba835cca14a9bbd99dd5c26b1b5fe6b1e304a
CodeLearner <kalki6309@gmail.com>
2024-08-24 10:24:00 +0530

    Update Topbar.js

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -8,12 +8,12 @@ const Topbar = () => {
         <header id="header" className="header sticky-top">
 
             <div className="topbar d-flex align-items-center">
-                <div className="container d-flex justify-content-center justify-content-md-between">
+                <div className="container d-flex flex-column flex-md-row justify-content-center justify-content-md-between">
                     <div className="contact-info d-flex align-items-center" >
                         <i className="bi bi-envelope d-flex align-items-center" ><a href="mailto:anushahospital2024@gmail.com">anushahospital2024@gmail.com</a></i>
                         <i className="bi bi-phone d-flex align-items-center ms-4"><span>+91 85220 08108</span></i>
                     </div>
-                    <div className="social-links d-none d-md-flex align-items-center">
+                    <div className="social-links d-flex align-items-center">
                         {/* <a href="#" className="twitter"><i className="bi bi-twitter-x"></i></a>
                         <a href="#" className="facebook"><i className="bi bi-facebook"></i></a>
                         <a href="#" className="instagram"><i className="bi bi-instagram"></i></a> */}
@@ -36,7 +36,6 @@ const Topbar = () => {
                             <li><a href="#gallery">Gallery</a></li>
                             <li><a href="#contact">Contact</a></li>
                         </ul>
-                      //  <i className="mobile-nav-toggle d-xl-none bi bi-list"></i>
                     </nav>
 
                     <a className="cta-btn d-none d-sm-block" style={{ backgroundColor: 'white', color: "#3fbbc0" }} href="#appointment">Make an Appointment</a>
